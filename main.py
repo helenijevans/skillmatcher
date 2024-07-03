bd5af484de521e70e95338414e472d37e7b9bb48
Helen Evans <helenijevans@gmail.com>
2024-07-03 11:21:23 +0100

    adding csv writing functionality

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import random
 from bs4 import BeautifulSoup
 from prettytable import PrettyTable
-import time
+import csv
 
 api_url = ('https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords={}&location=United'
            '%2BKingdom&geoId=101165590&trk=public_jobs_jobs-search-bar_search-submit&start={}')
@@ -15,14 +15,29 @@ print("* * * * * * * * * * * * * * * * * * * * * * * * * * \n"
       "  ││ │├┴┐└─┐  └─┐│ ││ │ ├┤  ││   │ │ │  └┬┘│ ││ │\n"
       " └┘└─┘└─┘└─┘  └─┘└─┘┴ ┴ └─┘─┴┘   ┴ └─┘   ┴ └─┘└─┘\n"
       "* * * * * * * * * * * * * * * * * * * * * * * * * *")
+print("ABOUT\nA program that finds UK-based jobs based on your skillset\n")
+print("HOW IT WORKS\nScrapes LinkedIn for jobs matching the combination of keywords, a table is returned after "
+      "processing and you will have the option to write it to a csv file for quick access.")
+print("* * * * * * * * * * * * * * * * * * * * * * * * * *\n")
 
-skill = input("Please enter a skill you have: ")
-print("\nThank you. You can take this time to grab a drink and relax - the job search has begun")
+done = False
+skill = ""
+
+print("Please enter all your job-related skills below, to exit - type `done`")
+
+while not done:
+    check = input("Skill: ")
+    if check.lower() == "done":
+        done = True
+    else:
+        skill += f"+{check}"
+
+print("\nThe job hunt has begun!")
 current_page = 0
 
 
 def load_jobs(page_no):
-    page = requests.get(api_url.format(skill, page_no))
+    page = requests.get( api_url.format(skill, page_no))
 
     soup = BeautifulSoup(page.content, "html.parser")
     return soup.find_all("div", class_="base-card")
@@ -36,7 +51,7 @@ unique_jobs = set()
 while jobs:
     output = random.randint(1, 1000)
     if output < 15:
-        print("Things are happening... please bear with")
+        print("Looks like there's a lot of job matches to your skillset... please bear with as we collate")
     jobsFound = True
     for job in jobs:
         unique_job_no = len(unique_jobs)
@@ -47,10 +62,18 @@ while jobs:
         if len(unique_jobs) > unique_job_no:
             job_link_element = job.find("a")['href']
             myTable.add_row([title_element, company_element, location_element, job_link_element])
-    jobs = load_jobs(current_page + 25)
+    current_page += 25
+    jobs = load_jobs(current_page)
 
 if not jobsFound:
     print("Sorry, no jobs were found for this criteria.")
 else:
     print(myTable)
-    print("\n\n** Done **")
+    writeToFile = input("\nWould you like to export this to a csv file? (y/n): ")
+    if writeToFile != "y":
+        print("\n\n** Done **")
+        exit()
+    else:
+        with open('skill_matched_jobs.csv', 'w', newline='') as f_output:
+            f_output.write(myTable.get_csv_string())
+            print("\n\n** Done **")
